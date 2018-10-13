9d855393ede7785968e6247bc1a0beaaab8705be
nanlei2000 <1456858184@qq.com>
2018-10-13 15:04:08 +0800

    delete font-css

---
+++ public/build/precache-manifest.217abf12ffbf24d6694031d3c6fa0cb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "faddd12fab1997f3a839",
+    "url": "/static/js/main.faddd12f.chunk.js"
+  },
+  {
+    "revision": "c0e88ec0eee0a28a8241",
+    "url": "/static/js/1.c0e88ec0.chunk.js"
+  },
+  {
+    "revision": "faddd12fab1997f3a839",
+    "url": "/static/css/main.643a6bbe.chunk.css"
+  },
+  {
+    "revision": "5b9f4cb6ae2eb78b396f51c98199488d",
+    "url": "/index.html"
+  }
+];
